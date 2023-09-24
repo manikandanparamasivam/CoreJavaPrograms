613bc45e8e0da6474ed3c15991c464480935d462
manikandanparamasivam <manikandanpckm@gmail.com>
2023-09-24 22:46:26 +0530

    its commited

---
+++ ArithOperation.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class ArithOperation
+{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        Scanner scan= new Scanner (System.in);
+        System.out.println("enter your a value");
+        a=scan.nextInt();
+        System.out.println("enter your ab value");
+        b=scan.nextInt();
+        c=a+b;
+       System.out.println("your add value="+c);
+    }
+}
