0346ed291a0fa264948174b0d632b83899b697d7
manikandanparamasivam <manikandanpckm@gmail.com>
2023-09-25 23:11:32 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class 
+{
+    public static void main(String[] args)
+    {
+        int a,b=1;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter your a value");
+        a=scan.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            b=b*i;
+        }
+        System.out.println("your factorial value"+b); 
+    }
+}
