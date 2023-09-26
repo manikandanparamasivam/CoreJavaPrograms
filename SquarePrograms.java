d5527d0e07e946fcd65ec7ad468114862846f038
manikandanparamasivam <manikandanpckm@gmail.com>
2023-09-26 17:34:04 +0530

    SquarePrograms

---
+++ SquarePrograms.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SquarePrograms
+{
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter your Square value");
+        a=scan.nextInt();
+        b=a;
+        for (int i=1;i<=1;i++)
+        {
+            b=a*b;
+            System.out.println("the square value="+b);
+        }
+
+    }
+
+}
