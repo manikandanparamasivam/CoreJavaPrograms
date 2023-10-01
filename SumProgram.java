bc72346529c809031786d89ea76563c4222cb0e7
manikandanparamasivam <manikandanpckm@gmail.com>
2023-10-01 15:37:06 +0530

    SumProgram

---
+++ SumProgram.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class SumProgram {
+    public static void main(String[] args) {
+        int a;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your value");
+        a = scan.nextInt();
+        System.out.println("your Sum value="+SumProgram.sumDigits(a));
+
+
+    }
+    public static int sumDigits(int a)
+    {
+         int r=0, sum=0;
+
+         while (a>0)
+         {
+             r=a%10;
+             sum=sum+r;
+             a=a/10;
+         }
+         return sum;
+    }
+}
