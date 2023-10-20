b31186020bf668a6c32badf469c92163e4b21946
manikandanparamasivam <manikandanpckm@gmail.com>
2023-10-20 22:33:52 +0530

    PatternProgram

---
+++ Basic Program/SquareFillPattern.java
@@ -0,0 +1,14 @@
+public class SquareFillPattern
+{
+    public static void main (String[] args){
+
+    int num=6;
+    for(int j=1;j<=num;j++){
+
+    for(int i=1;i<=num;i++){
+        System.out.print("*");
+    }
+    System.out.println("");
+    }
+    }
+}
