beeb0c9af96d8073166f17a0b3012c2f11ee7bea
manikandanparamasivam <manikandanpckm@gmail.com>
2023-11-15 23:35:21 +0530

    abstract program

---
+++ Abstrack/Man.java
@@ -0,0 +1,13 @@
+public class Man{
+    String name;
+    public void setname(){
+        this.name=name;
+    }
+    public String getname(){
+        return name;
+    }
+    public void eat(){
+        System.out.println("a man can eat ...food");
+    }
+    
+}
---
+++ Abstrack/Woman.java
@@ -0,0 +1,16 @@
+public class Woman
+{
+   private String name;
+   private Man a;
+     public void setname(){
+        this.name=name;
+    }
+     public String getname(){
+        return name;
+    }
+
+    public void work(){
+        System.out.println("a can do a work");
+    }
+    public void marry()
+}
