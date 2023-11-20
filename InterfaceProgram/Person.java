5171b07e6d58d6eeb9467f0e28a4609f7b1d8e59
manikandanparamasivam <manikandanpckm@gmail.com>
2023-11-20 17:57:06 +0530

    InterfaceProgram

---
+++ InterfaceProgram/Person.java
@@ -0,0 +1,5 @@
+public class Person{
+    public void testThing(Thing t){
+
+    }
+}
---
+++ InterfaceProgram/Vehicle.java
@@ -0,0 +1,8 @@
+public class Vehicle implements Thing
+{
+   String name;
+   public void doSomthing(){
+    System.out.println(" a vehicle stop");
+   }
+
+}
